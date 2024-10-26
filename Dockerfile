241dbce72abb71f5908976667e45007c4fab713a
Dilusha Gonagala <andrewdexis@gmail.com>
2024-10-26 15:17:01 -0600

    use the latest available on pip for awscli 1.35.15

--- Dockerfile
+++ Dockerfile
@@ -5,17 +5,19 @@ LABEL "com.github.actions.description"="Deploy static websites to an S3 bucket"
 LABEL "com.github.actions.icon"="upload-cloud"
 LABEL "com.github.actions.color"="blue"
 
-LABEL version="1.0.0"
+LABEL version="1.0.1"
 LABEL repository="https://github.com/andrewdex/s3-deploy-action"
 LABEL homepage="https://dilushagonagala.com/"
 LABEL maintainer="Dilusha Gonagala <hello@dilushagonagala.com>"
 LABEL "com.github.actions.license"="MIT"
 
-ENV AWSCLI_VERSION='2.18.15'
+ENV AWSCLI_VERSION='1.35.15'
 
 RUN apk add --no-cache curl \
     && pip install --quiet --no-cache-dir awscli==${AWSCLI_VERSION} \
     && apk del curl
+
+RUN aws --version
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Check if required environment variables are set
+# Function to check if required environment variables are set
 check_env_variable() {
   if [ -z "$1" ]; then
     echo "$2 is not set. Quitting."
@@ -10,6 +10,7 @@ check_env_variable() {
   fi
 }
 
+# Check required environment variables
 check_env_variable "$AWS_S3_BUCKET" "AWS_S3_BUCKET"
 check_env_variable "$AWS_ACCESS_KEY_ID" "AWS_ACCESS_KEY_ID"
 check_env_variable "$AWS_SECRET_ACCESS_KEY" "AWS_SECRET_ACCESS_KEY"
@@ -23,7 +24,13 @@ if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
 fi
 
+# CHECK AWS CLI version
+echo "Checking AWS CLI version..."
+aws --version
+
+
 # Configure AWS CLI with a dedicated profile
+echo "Configuring AWS CLI profile for s3-deploy-action..."
 aws configure --profile s3-deploy-action <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
@@ -31,24 +38,28 @@ ${AWS_REGION}
 text
 EOF
 
+# Log syncing details
+echo "Syncing files from ${SOURCE_DIR:-.} to s3://${AWS_S3_BUCKET}/${DEST_DIR}..."
+
 # Sync files to S3 bucket using the dedicated profile
 sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-deploy-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
 
-# clear out cloudfront cache if distribution id is not empty
-
+# Invalidate CloudFront cache if distribution ID is provided
 if [ -n "$CLOUDFRONT_DISTRIBUTION_ID" ]; then
-  echo "Invalidating CloudFront cache..."
+  echo "Invalidating CloudFront cache for distribution ID $CLOUDFRONT_DISTRIBUTION_ID..."
   aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" --profile s3-deploy-action
 fi
 
-
-# Clear out credentials after sync
+# Clean up AWS CLI profile
+echo "Cleaning up AWS CLI credentials..."
 aws configure --profile s3-deploy-action <<-EOF > /dev/null 2>&1
 null
 null
 null
 text
 EOF
+
+echo "Deployment completed successfully."
